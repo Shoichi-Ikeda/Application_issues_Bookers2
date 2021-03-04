a458af8563a35d70d2ebcebfc0cba9bc85d24aea
Shoichi-Ikeda <shoichann@gmail.com>
2021-03-04 13:49:07 +0000

    [Add] 検索機能

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,36 @@
+class SearchController < ApplicationController
+  def search
+		@book = Book.new
+		if params[:word].present? && params[:model] === "Book" && params[:search] === "perfect"
+			@books = Book.where('title LIKE ?', "#{params[:word]}") 
+			# 完全一致(book)
+		elsif params[:word].present? && params[:model] === "Book" && params[:search] === "front"
+			@books = Book.where('title LIKE ?', "#{params[:word]}%")
+			# 前方一致(book)
+		elsif params[:word].present? && params[:model] === "Book" && params[:search] === "back"
+			@books = Book.where('title LIKE ?', "%#{params[:word]}")
+			# 後方一致(book)
+		elsif params[:word].present? && params[:model] === "Book" && params[:search] === "part"
+			@books = Book.where('title LIKE ?', "%#{params[:word]}%")
+			# 部分一致(book)
+		else
+      @books = Book.none
+		end
+		
+		if params[:word].present? && params[:model] === "User" && params[:search] === "perfect"
+			@users = User.where('name LIKE ?', "#{params[:word]}")
+			# 完全一致(user)
+		elsif params[:word].present? && params[:model] === "User" && params[:search] === "front"
+			@users = User.where('name LIKE ?', "#{params[:word]}%")
+			# 前方一致(user)
+		elsif params[:word].present? && params[:model] === "User" && params[:search] === "back"
+			@users = User.where('name LIKE ?', "%#{params[:word]}")
+			# 後方一致(user)
+		elsif params[:word].present? && params[:model] === "User" && params[:search] === "part"
+			@users = User.where('name LIKE ?', "%#{params[:word]}%")
+			# 部分一致(user)
+		else
+      @users = User.none
+    end
+	end
+end
